dbe190792a024bc336e04440d7ce797a031c9005
SevvalKurtgoz <sevval1834@outlook.com>
2022-10-29 15:09:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let saveEl = document.getElementById("save-el")
+let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+if( gecmisBilgi !== null) {
+    saveEl.textContent = localStorage.getItem("gecmisBilgi")
+} else {
+    saveEl.textContent = ""
+}
+
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    result += count
+    total.textContent = result
+
+    localStorage.setItem( "gecmisBilgi", saveEl.textContent )
+
+     countEl.textContent = 0
+     count = 0
+ 
+}
+
